67a715ea6d4849d9e5b15ad1ffeef65602362055
Attila BODY <abody@logmein.com>
2015-10-06 16:26:29 +0200

    VS test session

--- src/gatedemo_test.cpp
+++ src/gatedemo_test.cpp
@@ -13,15 +13,52 @@
 
 using namespace std;
 
-unsigned long timetable_zeros[] = {
-	0, 436, 940, 444, 944, 440, 940, 440, 948, 436,944, 440, 944, 440,
-	944, 440, 944, 440, 944, 440, 944, 440, 944, 436,948, 436, 16156
+extern volatile unsigned int	g_inbuf;
+extern volatile bool			g_inputready;
+
+unsigned long timetable_combined[] = {
+	0,
+	120, 1300, 14000, 5,
+	436, 948, 436, 16156,
+	436, 940, 444, 944, 440, 940, 440, 948, 436, 944, 440, 944, 440,
+	944, 440, 944, 440, 944, 440, 944, 440, 944, 436, 948, 436, 16156,
+	432, 492, 892, 492, 892, 492, 896, 488, 892, 492, 896, 492, 892,
+	492, 892, 492, 892, 492, 892, 492, 892, 956, 428, 956, 428, 16168,
+	436, 948, 436, 948, 436, 952, 432, 948, 436, 948, 436, 488, 896,
+	488, 900, 484, 896, 492, 896, 488, 896, 948, 436, 952, 432, 16164,
 };
 
+unsigned long timetable_zeros[] = {
+	0,
+	120,1300,14000,5,
+	436,948,436,16156,
+	436,940,444,944,440,940,440,948,436,944,440,944,440,
+	944,440,944,440,944,440,944,440,944,436,948,436,16156
+};
+
+unsigned long timetable_ones[] = {
+	0,432,492,892,492,892,492,896,488,892,492,896,492,892,
+	492,892,492,892,492,892,492,892,956,428,956,428,16168
+};
+
+unsigned long timetable_mixed[] = {
+	0,436,948,436,948,436,952,432,948,436,948,436,488,896,
+	488,900,484,896,492,896,488,896,948,436,952,432,16164
+};
+
+#define TIMETABLE timetable_combined
+
 int main() {
-	set_time_table_delta( timetable_zeros, ITEMCOUNT( timetable_zeros ));
-	for( unsigned int cycle = 0; cycle < ITEMCOUNT( timetable_zeros) - 1 ; ++cycle ) {
+
+	cout << hex;
+	set_time_table_delta( TIMETABLE, ITEMCOUNT( TIMETABLE ));
+	for( unsigned int cycle = 0; cycle < ITEMCOUNT( TIMETABLE ) - 1 ; ++cycle ) {
 		isr();
+		if( g_inputready ) {
+			cout << g_inbuf << endl;
+			g_inputready = false;
+		}
 	}
+
 	return 0;
 }
