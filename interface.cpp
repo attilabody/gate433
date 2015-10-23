e6d77df3c37cd61e548523ae239c98deff585717
Attila BODY <attila.body@gmail.com>
2015-10-23 22:12:45 +0200

    buttons

--- interface.cpp
+++ interface.cpp
@@ -2,11 +2,12 @@
 #include "interface.h"
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_TFTLCD.h> // Hardware-specific library
+#include <TouchScreen.h>
 #include <SPI.h>
 #include <SD.h>
 
 #define ITEMCOUNT(A) (sizeof(A)/sizeof(A[0]))
-#define BAUDRATE 57600
+#define BAUDRATE 115200
 #define RESP ":"
 
 // The control pins for the LCD can be assigned to any digital or
@@ -47,12 +48,30 @@
 #define MAGENTA 0xF81F
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
+#define GREY	0x7BEF
+#define D_GREEN	0x03E0
+#define D_GREY	0x39e7
 
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+#define YP A3  // must be an analog pin, use "An" notation!
+#define XM A2  // must be an analog pin, use "An" notation!
+#define YM 9   // can be a digital pin
+#define XP 8   // can be a digital pin
+
+#define TS_MINX 189
+#define TS_MINY 174
+#define TS_MAXX 900
+#define TS_MAXY 950
+
+TouchScreen	g_ts( XP, YP, XM, YM, 258 );
+int16_t		g_bottom( TS_MINY ), g_left( TS_MINX ), g_top( TS_MAXY), g_right( TS_MAXX );
+bool		g_owner( true );
 
 void	printCode( int code );
 void	processInput();
 long	getintparam(unsigned char &sbindex, bool decimal = true );
+void	drawbuttons( bool first, uint16_t color_h = D_GREEN, uint16_t color_l = D_GREY );
 
 #ifdef VERBOSE
 template< typename T1, typename T2 > void dbgout( const T1 &a, const T2 &b, bool newline = true )
@@ -65,22 +84,26 @@ template< typename T1, typename T2 > void dbgout( const T1 &a, const T2 &b, bool
 }
 #endif
 
-char 			g_serbuf[64];
+char 			g_serbuf[256];
 unsigned char	g_serptr(0);
 const char 		*g_commands[] = {
 	  "GET"
 	, "SET"
 	, "SETF"
 	, "SHOW"
+	, "LOG"
+	, "BTN"
 };
 
+uint16_t	g_buttonheight, g_buttonwidth, g_buttontop;
 
+//////////////////////////////////////////////////////////////////////////////
 void setup(void)
 {
 	Serial.begin( BAUDRATE);
 	tft.reset();
 	tft.begin(tft.readID());
-	tft.setRotation(0);
+	tft.setRotation(2);
 	delay(100);
 	tft.fillScreen( BLACK);
 
@@ -92,8 +115,78 @@ void setup(void)
 		tft.setTextColor(GREEN);
 		tft.println("SD OK");
 	}
+	g_buttonheight = 40;
+	g_buttontop = tft.height() - g_buttonheight;
+	g_buttonwidth = tft.width() / 2;
+
+	drawbuttons( true );
+	drawbuttons( true );
 }
 
+//////////////////////////////////////////////////////////////////////////////
+void loop(void)
+{
+	while (Serial.available())
+	{
+		char inc = Serial.read();
+		g_serbuf[g_serptr++] = ( inc == '\n' ? 0 : inc );
+		if (inc == '\n' || g_serptr >= sizeof( g_serbuf ) -1 ) {
+			g_serbuf[g_serptr] = 0;
+			processInput();
+			g_serptr = 0;
+		}
+	}
+	//digitalWrite(13, HIGH);
+	TSPoint p = g_ts.getPoint();
+	//digitalWrite(13, LOW);
+
+	// if sharing pins, you'll need to fix the directions of the touchscreen pins
+	//pinMode(XP, OUTPUT);
+	pinMode(XM, OUTPUT);
+	pinMode(YP, OUTPUT);
+
+	if (p.z > g_ts.pressureThreshhold)
+	{
+		if( p.x < g_left ) g_left = p.x;
+		if( p.y < g_bottom ) g_bottom = p.y;
+		if( p.x > g_top ) g_top = p.x;
+		if( p.y > g_right ) g_right = p.y;
+
+		float x( (float)(p.x - g_left)/(float)(g_right - g_left) );
+		float y( (float)(p.y - g_bottom)/(float)(g_top - g_bottom) );
+
+		if( y > 0.82 )
+		{
+			bool owner( x < 0.5 );
+			if( owner != g_owner ) {
+				drawbuttons( owner);
+				g_owner = owner;
+			}
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////
+void drawbuttons( bool first, uint16_t color_h, uint16_t color_l )
+{
+	tft.fillRect(0, g_buttontop, g_buttonwidth, g_buttonheight, first ? color_h : color_l );
+	tft.fillRect(g_buttonwidth, g_buttontop, g_buttonwidth, g_buttonheight, first ? color_l : color_h );
+	tft.setTextSize(3);
+	tft.setCursor( 27, g_buttontop + 8 ); tft.setTextColor( first ? WHITE : GREY ); tft.print( "lako" );
+	tft.setCursor( g_buttonwidth + 17, g_buttontop + 8); tft.setTextColor( first ? GREY : WHITE ); tft.print("berlo");
+}
+
+//////////////////////////////////////////////////////////////////////////////
+void printCode( int code )
+{
+	tft.fillRect( 0, 0, 240, 40, BLACK );
+	tft.setCursor( 0, 0 );
+	tft.setTextColor( WHITE );
+	tft.setTextSize( 5 );
+	tft.println( code, DEC );
+}
+
+//////////////////////////////////////////////////////////////////////////////
 inline char convertdigit( char c, bool decimal = true )
 {
 	if( decimal )
@@ -106,6 +199,7 @@ inline char convertdigit( char c, bool decimal = true )
 	}
 }
 
+//////////////////////////////////////////////////////////////////////////////
 long getintparam(unsigned char &sbidx, bool decimal)
 {
 	long	retval(0);
@@ -128,32 +222,10 @@ long getintparam(unsigned char &sbidx, bool decimal)
 	return found ? retval : -1;
 }
 
-void loop(void)
-{
-	while (Serial.available())
-	{
-		char inc = Serial.read();
-		g_serbuf[g_serptr++] = ( inc == '\n' ? 0 : inc );
-		if (inc == '\n' || g_serptr >= sizeof( g_serbuf ) -1 ) {
-			g_serbuf[g_serptr] = 0;
-			processInput();
-			g_serptr = 0;
-		}
-	}
-}
-
-void printCode( int code )
-{
-	tft.fillRect( 0, 0, 240, 40, BLACK );
-	tft.setCursor( 0, 0 );
-	tft.setTextColor( WHITE );
-	tft.setTextSize( 5 );
-	tft.println( code, DEC );
-}
-
+//////////////////////////////////////////////////////////////////////////////
 char findcommand(unsigned char &inptr)
 {
-	while (inptr < g_serptr && g_serbuf[inptr] != ' ' && g_serbuf[inptr] != ','
+	while (inptr < g_serptr && g_serbuf[inptr] && g_serbuf[inptr] != ' ' && g_serbuf[inptr] != ','
 			&& g_serbuf[inptr] != '\n')
 		++inptr;
 
@@ -174,8 +246,11 @@ char findcommand(unsigned char &inptr)
 	return -1;
 }
 
+//////////////////////////////////////////////////////////////////////////////
 void processInput()
 {
+//	Serial.println( g_serbuf );
+
 	static char 	linebuffer[25];
 	unsigned char	inptr(0);
 	char			command( findcommand(inptr) );
@@ -260,6 +335,14 @@ void processInput()
 		code = getintparam(inptr);
 		printCode( code );
 		Serial.println( RESP "OK" );
+		break;
+
+	case 4:		//	LOG Text to log
+		Serial.println( RESP "OK" );
+		break;
+
+	case 5:		//	BTN Text to log
+		Serial.println( g_owner ? RESP "0" : RESP "1" );
 		break;
 	}
 }
