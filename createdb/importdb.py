1a79e94621cffefa72d0ba30ca0c1aa720c91625
Attila BODY <attila.body@gmail.com>
2016-02-01 17:26:38 +0100

    importdb tenant time range changes

--- createdb/importdb.py
+++ createdb/importdb.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-tenant_in_start = int(7.5*60)
-tenant_out_start = int(7.5*60)
-tenant_in_end = int(18*60)
-tenant_out_end = int(18.5*60)
+tenant_in_start = 7 * 60 + 30
+tenant_out_start = 7 * 60 + 30
+tenant_in_end = 17 * 60 + 30
+tenant_out_end = 18 * 60 + 5
 
 active = {}
 with open('owners.txt', 'r') as infile:
     for line in infile:
         sline = line.strip()
-        if( len(sline) > 0 and not sline.startswith('#') ):
-#            print( len( sline), ' ', sline)
+        if len(sline) > 0 and not sline.startswith('#'):
+            # print( len( sline), ' ', sline)
             active[int(sline)] = 1
 
-with open('tenants.txt', 'r' ) as infile:
+with open('tenants.txt', 'r') as infile:
     for line in infile:
         sline = line.strip()
-        if( len(sline) > 0 and not sline.startswith('#') ):
-#            print( len( sline), ' ', sline)
+        if len(sline) > 0 and not sline.startswith('#'):
+            # print( len( sline), ' ', sline)
             active[int(sline)] = 2
 
-with open( 'DB.TXT', 'w' ) as dbfile:
-    for id in range(0,1024):
+with open('DB.TXT', 'w') as dbfile:
+    for id in range(0, 1024):
         if id in active:
             if active[id] == 1:
                 dbline = '000 FFF 000 FFF 07F 000\n'
@@ -29,6 +29,5 @@ with open( 'DB.TXT', 'w' ) as dbfile:
                          (tenant_in_start, tenant_in_end, tenant_out_start, tenant_out_end)
         else:
             dbline = '000 000 000 000 000 000\n'
-        print( dbline, end='' )
-        dbfile.write( dbline )
-
+        print(dbline, end='')
+        dbfile.write(dbline)
