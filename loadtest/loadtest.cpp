874f22a56e5917aa585c74d13799558a049fb56d
Attila BODY <attila.body@gmail.com>
2016-02-06 17:51:30 +0100

    loadtest: simple but working already

--- loadtest/loadtest.cpp
+++ loadtest/loadtest.cpp
@@ -1,7 +1,8 @@
 // Do not remove the include beSTATE2
+#include <Arduino.h>
+#include <TimerOne.h>
 #include "loadtest.h"
 
-#include <TimerOne.h>
 
 #define BAUDRATE 115200
 #define ITEMCOUNT(A) (sizeof(A)/sizeof(A[0]))
@@ -9,6 +10,11 @@
 
 const uint8_t g_outPin( 3 );
 const uint8_t g_ledPin( LED_BUILTIN );
+const uint8_t g_loopin( 4 );
+const uint8_t g_loopout( 5 );
+
+#define	LOOP_ACTIVE	LOW
+#define LOOP_INACTIVE HIGH
 
 volatile bool	g_inputavail( false );
 unsigned int	g_input;
@@ -24,12 +30,79 @@ void setup()
 {
 	pinMode(g_ledPin, OUTPUT);
 	pinMode(g_outPin, OUTPUT);
+	pinMode(g_loopin, OUTPUT);
+	pinMode(g_loopout, OUTPUT);
+
+	digitalWrite( g_loopin, LOOP_INACTIVE );
+	digitalWrite( g_loopout, LOOP_INACTIVE );
 
 	Timer1.initialize( CYCLETIME );
 	Timer1.attachInterrupt( isr );
 
 	Serial.begin(BAUDRATE);
 }
+
+void printio( bool inner )
+{
+	Serial.print( inner ? F("inner ") : F("outer ") );
+}
+
+const char PROGMEM g_innerstr[] = "inner";
+const char PROGMEM g_outerstr[] = "outer";
+
+// The loop function is called in an endless loop
+void loop()
+{
+	static bool ledStatus( true );
+	static bool direction;
+
+	for( unsigned int code = 0; code < (1024); ++code )
+	{
+		Serial.print( F("<-------> Starting loop "));
+		Serial.println( code );
+		bool dir(((bool)(code & 1)) == direction);
+
+		if( !(code & 0xf )) {
+			digitalWrite( g_ledPin, ledStatus ? HIGH : LOW );
+			ledStatus = !ledStatus;
+		}
+
+
+		digitalWrite( dir ? g_loopin : g_loopout, LOOP_ACTIVE );
+		Serial.print(F("Activating "));
+//		Serial.print(F( dir ? g_innerstr : g_outerstr ));
+		printio(dir);
+		Serial.println(F("loop."));
+		delay( 2000 );
+
+		Serial.println(F("Sending code."));
+		for( int retry = 0; retry < 4; ++retry ) {
+			while( g_inputavail );
+			g_input = code << 2;
+			g_inputavail = true;
+		}
+		Serial.println(F("Code sent."));
+		delay( 2000 );
+
+		Serial.print(F("Activating "));
+		printio(!dir);
+		Serial.println(F("loop."));
+		digitalWrite( !dir ? g_loopin : g_loopout, LOOP_ACTIVE );
+		delay( 100 );
+		Serial.print(F("Deactivating "));
+		printio(dir);
+		Serial.println(F("loop."));
+		digitalWrite( dir ? g_loopin : g_loopout, LOOP_INACTIVE );
+		delay( 2000 );
+		Serial.print(F("Deactivating "));
+		printio(!dir);
+		Serial.println(F("loop."));
+		digitalWrite( !dir ? g_loopin : g_loopout, LOOP_INACTIVE );
+		delay( 5000 );
+	}
+	direction = ! direction;
+}
+
 
 void isr()
 {
@@ -76,21 +149,3 @@ void isr()
 }
 
 
-// The loop function is called in an endless loop
-void loop()
-{
-	static bool ledStatus( true );
-
-	for( unsigned int code = 0; code < (1<<12); code += 4 ) {
-		if( !((code >> 2) & 0xf )) {
-			digitalWrite( g_ledPin, ledStatus ? HIGH : LOW );
-			ledStatus = !ledStatus;
-		}
-		for( int retry = 0; retry < 4; ++retry ) {
-			while( g_inputavail );
-			g_input = code;
-			g_inputavail = true;
-			Serial.println( code );
-		}
-	}
-}
