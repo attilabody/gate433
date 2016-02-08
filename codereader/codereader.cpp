546959367495720bb30c1cb516a425ea12d50f0e
Attila BODY <attila.body@gmail.com>
2016-02-08 21:51:16 +0100

    codereader: adding SD log

--- codereader/codereader.cpp
+++ codereader/codereader.cpp
@@ -1,12 +1,22 @@
 // Do not remove the include below
+#include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 #include "codereader.h"
 #include "config.h"
 #include "decode433.h"
+#include "SdFat.h"
+#include "sdfatlogwriter.h"
+#include "ds3231.h"
+
+SdFat			g_sd;
+sdfatlogwriter	g_logger( g_sd );
 
 LiquidCrystal_I2C	g_lcd( LCD_I2C_ADDRESS, LCD_WIDTH, LCD_HEIGHT );
 
 //The setup function is called once at startup of the sketch
+
+ts		g_dt;
+
 void setup()
 {
 	Serial.begin( BAUDRATE );
@@ -21,6 +31,13 @@ void setup()
 	g_lcd.init();
 	g_lcd.backlight();
 
+	if( g_sd.begin( SS, SPI_HALF_SPEED )) {
+		if( !g_logger.init() )
+			Serial.println(F("Logger fail"));
+	} else
+		Serial.println(F("SD fail"));
+
+	g_logger.log( logwriter::DEBUG, g_dt, F("Start"));
 }
 
 // The loop function is called in an endless loop
@@ -32,13 +49,15 @@ void loop()
 	if( g_codeready )
 	{
 		if( code != g_code ) {
-			Serial.print( F("Aborting ") );
-			Serial.print( code );
-			Serial.print( ' ' );
-			Serial.println( g_code );
+			if( cnt ) {
+				Serial.print( F("Aborting ") );
+				Serial.print( code );
+				Serial.print( ' ' );
+				Serial.println( g_code );
+			}
 			code = g_code;
 			cnt = 0;
-		} else if( cnt++ > 2 ) {
+		} else if( cnt++ > 1 ) {
 			uint16_t	id( getid( g_code ));
 			Serial.println( id );
 			g_lcd.setCursor(0,0);
@@ -49,8 +68,9 @@ void loop()
 			g_lcd.print('.');
 			g_lcd.print( getbutton( g_code ) );
 			g_lcd.print(F("   "));
+			g_logger.log( logwriter::INFO, g_dt, F("Remote"), id, getbutton( g_code ));
 			cnt = 0;
-		}
+		} else Serial.print('.');
 		g_codeready = false;
 	}
 }
