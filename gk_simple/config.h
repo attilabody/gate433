04195dc6f8950ca59abe1de56731af16e99de312
Attila BODY <attila.body@gmail.com>
2016-02-08 11:04:57 +0100

    gk_simple: needs 4 times the same code to open the gate

---
+++ gk_simple/config.h
@@ -0,0 +1,21 @@
+/*
+ * config.h
+ *
+ *  Created on: Feb 6, 2016
+ *      Author: compi
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define BAUDRATE 115200
+
+#define DECODE433_REVERSE
+
+#define PIN_RFIN		2
+#define PIN_GATE		A2
+
+#define RELAY_OFF	HIGH
+#define RELAY_ON	LOW
+
+#endif /* CONFIG_H_ */
--- gk_simple/gk_simple.cpp
+++ gk_simple/gk_simple.cpp
@@ -1,15 +1,49 @@
 // Do not remove the include below
 #include "gk_simple.h"
-
+#include "decode433.h"
+#include "toolbox.h"
+#include "commsyms.h"
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+	Serial.begin( BAUDRATE );
+	delay(10);
+	Serial.print( CMNT );
+	for( char c = 0; c < 79; ++c ) Serial.print('-');
+	Serial.println();
+
+	pinMode( PIN_GATE, OUTPUT );
+	digitalWrite( PIN_GATE, RELAY_OFF );
+	setup433();
+	g_codeready = false;
+	g_code = 0;
+
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
+	static uint16_t	code(0);
+	static uint8_t	cnt(0);
+
+	if( g_codeready )
+	{
+		if( code != g_code ) {
+			Serial.print( F("Aborting ") );
+			Serial.print( code );
+			Serial.print( ' ' );
+			Serial.println( g_code );
+			code = g_code;
+			cnt = 0;
+		} else if( cnt++ > 3 ) {
+			Serial.print( g_code >> 2 );
+			Serial.println(F(" -> opening gate."));
+			digitalWrite( PIN_GATE, RELAY_ON );
+			delay(1000);
+			digitalWrite( PIN_GATE, RELAY_OFF );
+			cnt = 0;
+		}
+		g_codeready = false;
+	}
 }
