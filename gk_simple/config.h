0850d666e9daefc59d0f8f8d1510e1e2b3d85912
Attila BODY <attila.body@gmail.com>
2016-02-10 16:01:06 +0100

    gk_simple: Fixing traffic lights pins (inside<->outside)

--- gk_simple/config.h
+++ gk_simple/config.h
@@ -18,11 +18,11 @@
 #define RELAY_OFF	HIGH
 #define RELAY_ON	LOW
 
-#define OUT_GREEN	9
-#define OUT_YELLOW	8
-#define	OUT_RED		7
-#define IN_GREEN	6
-#define IN_YELLOW	5
-#define IN_RED		4
+#define OUT_GREEN	6
+#define OUT_YELLOW	5
+#define	OUT_RED		4
+#define IN_GREEN	9
+#define IN_YELLOW	8
+#define IN_RED		7
 
 #endif /* CONFIG_H_ */
