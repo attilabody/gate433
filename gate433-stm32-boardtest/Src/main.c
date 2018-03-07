d6d462ed51ac0f464aa2b43be33cfd9c7280f477
Attila BODY <attila.body@gmail.com>
2018-03-07 22:35:46 +0100

    stm32 board test: added stm32goodies

--- gate433-stm32-boardtest/Src/main.c
+++ gate433-stm32-boardtest/Src/main.c
@@ -39,7 +39,9 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f1xx_hal.h"
+#include "i2c.h"
 #include "rtc.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* USER CODE BEGIN Includes */
@@ -91,6 +93,8 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_RTC_Init();
+  MX_I2C1_Init();
+  MX_USART1_UART_Init();
 
   /* USER CODE BEGIN 2 */
 
